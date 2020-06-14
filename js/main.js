a8640a2536bf941f409da717b544ea435bb2cb64
rahulkay89 <rahulkay89@gmail.com>
2020-06-14 09:23:13 +0530

    Finished a basic landing page.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ $(document).ready(function()
 {
   $('.header').height($(window).height());
 })
+
+/* Scroll effect */
+$(".navbar a").click(function()
+{
+  $("body,html").animate(
+  {
+    scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+})
