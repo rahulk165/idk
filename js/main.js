78a373922040a7549f45b83adced1a7b14bd6b4a
rahulkay89 <rahulkay89@gmail.com>
2020-06-14 21:11:32 +0530

    Added smooth scroll effect.

--- js/main.js
+++ js/main.js
@@ -2,12 +2,3 @@ $(document).ready(function()
 {
   $('.header').height($(window).height());
 })
-
-/* Scroll effect */
-$(".navbar a").click(function()
-{
-  $("body,html").animate(
-  {
-    scrollTop:$("#" + $(this).data('value')).offset().top
-  },1000)
-})
