0e26bfa00fab2d497a443903b2c9c6583c0236d7
rahulkay89 <rahulkay89@gmail.com>
2020-06-19 10:51:01 +0530

    Added one more course ( Foundation program ).

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function()
 {
   $('.header').height($(window).height());
+
+  /* To close the toggle menu when clicking navigation item anchor link */
+  /* Source : https://stackoverflow.com/questions/26383681/how-to-close-toggle-menu-when-clicking-navigation-item-anchor-link */
+  /*
+  $('#menu li').on('click', function(){
+    $("#menu").hide();
+    $("#menu-icon").removeClass("active");
+  });
+  */
 })
